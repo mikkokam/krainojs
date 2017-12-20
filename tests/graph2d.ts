8e26a7e6bf6efe9a16161293f7bb12599490006d
Mikko Kamarainen <mikko@goodhum.com>
2017-12-20 23:15:18 +0200

    Test for conv2D and maxPooling2D

--- tests/graph2d.ts
+++ tests/graph2d.ts
@@ -3,12 +3,13 @@ import { Layers } from "../src/layers";
 
 
 let model = new Sequential();
-model.add(Layers.input([2,2,1]));
+model.add(Layers.input([4,4,1]));
 model.add(Layers.activation('relu'));
-model.add(Layers.conv2D(2,{outputDepth: 4}));
+model.add(Layers.conv2D(2,{outputDepth: 8}));
 model.add(Layers.activation('relu'));
+model.add(Layers.maxPooling2D(2));
 model.add(Layers.flatten());
-model.add(Layers.dense(2,{activation: 'sigmoid'}));
+model.add(Layers.dense(4,{activation: 'sigmoid'}));
 model.add(Layers.output(1));
 
 console.log(model);
@@ -20,8 +21,8 @@ model.compile({
 
 model.fit({
     input: [
-        [[[0],[0]],[[0],[1]]],
-        [[[1],[0]],[[1],[1]]]
+        [[[0],[0],[0],[0]],[[0],[1],[0],[1]],[[0],[0],[0],[0]],[[0],[1],[0],[1]]],
+        [[[1],[0],[1],[0]],[[1],[1],[1],[1]],[[1],[0],[1],[0]],[[1],[1],[1],[1]]]
     ],
     target: [
         [0],
@@ -31,7 +32,7 @@ model.fit({
 })
 .then(() => {
     return model.predict({
-        input: [[[1],[0]],[[1],[1]]]
+        input: [[[0],[0],[0],[0]],[[0],[1],[0],[1]],[[0],[0],[0],[0]],[[0],[1],[0],[1]]],
     })
 })
 .then(res => {
